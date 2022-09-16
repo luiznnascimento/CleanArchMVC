0f176992cfda639bd5a6818e98fd25083f3e3ae5
Luiz Henrique Nunes Nascimento <luiz.nascimento@N095187.local>
2022-09-16 09:26:46 -0300

    feat: iniciando a criacao do projeto Infra.Data

---
+++ CleanArchMVC.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+using CleanArchMVC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMVC.Infra.Data.EntitiesConfiguration;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(t => t.Id);
+        builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+        
+        builder.HasData(
+            new Category(1, "Eletronicos"),
+            new Category(2, "Livros"),
+            new Category(3, "Bebidas")
+        );
+    }
+}
---
+++ CleanArchMVC.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+using CleanArchMVC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMVC.Infra.Data.EntitiesConfiguration;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(t => t.Id);
+        
+        builder.Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+        builder.Property(p => p.Description)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(p => p.Price)
+            .HasPrecision(10, 2);
+        
+        builder.HasOne(e => e.Category)
+            .WithMany(e => e.Products)
+            .HasForeignKey(e => e.CategoryId);
+    }
+}
